eb56e9be176a452d8a39c0505ea64f247c386424
Liu Yiqun <liuyiqun01@baidu.com>
2025-02-18 10:43:46 +0800

    Correct testing script.

--- tests/ap/paddle-tests/test_matmul_binary.py
+++ tests/ap/paddle-tests/test_matmul_binary.py
@@ -64,7 +64,7 @@ class TestAPMatmulBinary(unittest.TestCase):
         self.b = paddle.randn(self.b_shape, dtype=self.dtype)
         self.b.stop_gradient = False
 
-    def eval_symbolic(self, use_cinn, profile, backward=False):
+    def eval_symbolic(self, use_cinn, profile):
         net = CINNSubGraphNet()
         input_spec = [
             InputSpec(shape=self.x_shape, dtype=self.dtype),
