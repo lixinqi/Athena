509ba209ac83ddda125a3f95b837ae779ca41846
Liu Yiqun <liuyiqun01@baidu.com>
2025-04-17 18:51:02 +0800

    Rename Wrapper to Runner.

--- tests/ap/matmul/autotune.h
+++ tests/ap/matmul/autotune.h
@@ -6,21 +6,21 @@
 
 namespace ap {
 
-template <typename Wrapper, int... Is>
+template <typename Runner, int... Is>
 auto GenerateFuncList(std::integer_sequence<int, Is...>) {
-  using FuncPtr = decltype(&Wrapper::template Apply<0>);
-  return std::vector<FuncPtr>{&Wrapper::template Apply<Is>...};
+  using FuncPtr = decltype(&Runner::template Apply<0>);
+  return std::vector<FuncPtr>{&Runner::template Apply<Is>...};
 }
 
-template <typename T, typename Wrapper, typename... Args>
+template <typename T, typename Runner, typename... Args>
 int RunWithAutotune(cudaStream_t stream, int config_id, Args &&...args) {
   int selected_config_id = config_id;
 
 #if AP_ENABLE_AUTOTUNE
-  using FuncPtr = decltype(&Wrapper::template Apply<0>);
+  using FuncPtr = decltype(&Runner::template Apply<0>);
   constexpr int N = ap::ConfigsInfo<T>::kNumTotals;
   static std::vector<FuncPtr> matmul_functions =
-      GenerateFuncList<Wrapper>(std::make_integer_sequence<int, N>{});
+      GenerateFuncList<Runner>(std::make_integer_sequence<int, N>{});
   if (selected_config_id == -1) {
     selected_config_id = ap::ProfileBestConfig(matmul_functions, stream,
                                                std::forward<Args>(args)...);
